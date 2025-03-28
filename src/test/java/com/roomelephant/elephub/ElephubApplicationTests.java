45ae7bca170c4bd8508657900cbd044fc74035ae
JPedroBorges <JPedroBorges@users.noreply.github.com>
2025-03-28 00:07:27 +0000

    first commit

---
+++ src/test/java/com/roomelephant/elephub/ElephubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomelephant.elephub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElephubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
