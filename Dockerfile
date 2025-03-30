dcf444809ecadad9fd95e8df42cd617fc9ce6021
JPedroBorges <JPedroBorges@users.noreply.github.com>
2025-03-30 03:18:54 +0100

    add base docker connection

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,13 @@
 FROM eclipse-temurin:21-jre
 
-RUN groupadd -r elephubGroup \
-    && useradd -r -g elephubGroup elephubUser
-
 # Set working directory
 WORKDIR /opt
 
 # Copy application jar
-COPY target/elephub-0.0.1-SNAPSHOT.jar elephub-service/app.jar
+COPY target/elephub.jar elephub-service/app.jar
 
 # Copy entrypoint.sh script
 COPY entrypoint.sh entrypoint.sh
 RUN chmod +x entrypoint.sh
 
-USER elephubUser
 ENTRYPOINT ["./entrypoint.sh"]
--- src/main/java/com/roomelephant/elephub/Main.java
+++ src/main/java/com/roomelephant/elephub/Main.java
@@ -4,31 +4,68 @@ import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
-import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.okhttp.OkDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
-import com.github.dockerjava.transport.SSLConfig;
-import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.time.Duration;
+import java.nio.file.Path;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    public static final String DOCKER_SOCKET = "/var/run/docker.sock";
+
     public static void main(String[] args) {
-        System.out.println("Elephub Application started");
+        log.info("operation='main', message='Elephub Application started'");
 
-        DockerClientConfig custom = DefaultDockerClientConfig.createDefaultConfigBuilder()
-                .withDockerHost("unix:///var/run/docker.sock")
+        Path socketPath = Path.of(DOCKER_SOCKET);
+        if (hasInvalidPath(socketPath)) {
+            log.error("operation='main', message='Docker socket not found at  {}'",
+                    DOCKER_SOCKET);
+            System.exit(1);
+        }
+
+        if (hasInvalidPermissions(socketPath)) {
+            log.error("operation='main', message='Insufficient permissions to access Docker socket at {}', user='{}', permissions='{}'",
+                    DOCKER_SOCKET, System.getProperty("user.name"), socketPath.toFile().getAbsolutePath());
+            System.exit(1);
+        }
+
+        DockerClient dockerClient = getDockerClient();
+
+        try {
+            log.debug("operation='main', message='Attempting to connect to Docker daemon...'");
+            dockerClient.pingCmd().exec();
+        } catch (Exception e) {
+            log.error("operation='main', message='Failed to connect to Docker daemon: {}'",
+                    e.getMessage(), e);
+            System.exit(1);
+        }
+
+        dockerClient.listContainersCmd().exec().forEach(container ->
+                System.out.println("Container: " + container.getImage()));
+
+    }
+
+    private static DockerClient getDockerClient() {
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost("unix://" + DOCKER_SOCKET)
+                .withDockerTlsVerify(false)
                 .build();
 
-        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
-                .dockerHost(custom.getDockerHost())
-                .sslConfig(custom.getSSLConfig())
-                .maxConnections(100)
-                .connectionTimeout(Duration.ofSeconds(30))
-                .responseTimeout(Duration.ofSeconds(45))
+        DockerHttpClient httpClient = new OkDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
                 .build();
 
-        DockerClient dockerClient = DockerClientImpl.getInstance(custom, httpClient);
-        dockerClient.pingCmd().exec();
+        DockerClient dockerClient = DockerClientImpl.getInstance(config, httpClient);
+        return dockerClient;
+    }
+
+    private static boolean hasInvalidPath(Path socketPath) {
+        return !socketPath.toFile().exists();
+    }
+
+    private static boolean hasInvalidPermissions(Path socketPath) {
+        return !socketPath.toFile().canRead() || !socketPath.toFile().canWrite();
     }
 }
