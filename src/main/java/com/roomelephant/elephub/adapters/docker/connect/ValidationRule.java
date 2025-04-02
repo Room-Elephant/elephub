88a78bdb199efd1421fb59f35158a3916527cc1e
JPedroBorges <JPedroBorges@users.noreply.github.com>
2025-04-02 18:20:12 +0100

    improve code readability (#16)

--- src/main/java/com/roomelephant/elephub/adapters/docker/connect/ValidationRule.java
+++
@@ -1,8 +0,0 @@
-package com.roomelephant.elephub.adapters.docker.connect;
-
-import com.roomelephant.elephub.util.ExcludeFromJacocoGeneratedReport;
-import java.util.function.Predicate;
-
-@ExcludeFromJacocoGeneratedReport
-record ValidationRule<T>(Predicate<T> validator, String errorMessage) {
-}
